2f607e21d19b4270b8fdfa0ca75113e7f612b31b
AdzDa <adzruldani@gmail.com>
2023-10-02 22:10:03 +0800

    added Assignment 1

---
+++ Homework/Topic1/solution.py
@@ -0,0 +1,62 @@
+#8 = H
+beeps = 2
+boops = 6
+total = beeps + boops
+print(total)
+
+#5 = E
+beeps = 0
+boops = 5
+total = beeps + boops
+print(total)
+
+#12 = L
+beeps = 9
+boops = 3
+total = beeps + boops
+print(total)
+
+#12 = L
+beeps = 4
+boops = 8
+total = beeps + boops
+print(total)
+
+#15 = O
+beeps = 10
+boops = 5
+total = beeps + boops
+print(total)
+
+#23 = W
+beeps = 11
+boops = 12
+total = beeps + boops
+print(total)
+
+#Editted/ changed from (5 beeps & 5 boops) into (10 beeps & 5 boops)
+#15 = O
+beeps = 10
+boops = 5
+total = beeps + boops
+print(total)
+
+#18 = R
+beeps = 1
+boops = 17
+total = beeps + boops
+print(total)
+
+#12 = L
+beeps = 5
+boops = 7
+total = beeps + boops
+print(total)
+
+#4 = D
+beeps = 4
+boops = 0
+total = beeps + boops
+print(total)
+
+print("ANSWER: HELLO WORLD")
